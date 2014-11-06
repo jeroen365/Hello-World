84b83cb38f63fd95a170f000a2a1717c02a72a5a
jeroen365 <jeroenvanderes@gmail.com>
2014-11-06 13:39:09 +0100

    Hoi

---
+++ Hello World/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Jeroen van der Es on 02-11-14.
+//  Copyright (c) 2014 Jeroen van der Es. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
